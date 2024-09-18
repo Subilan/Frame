d23503bde077f12f73474a9886d5bad74b0a7343
Subilan <suyuanliang1995@gmail.com>
2024-09-18 18:18:35 +0800

    chore: using base64

--- server/api/get-exif.ts
+++ server/api/get-exif.ts
@@ -3,12 +3,14 @@ import exifs from '@/server/data/exifs.json';
 
 export default defineEventHandler(e => {
     const query = getQuery(e);
-    const name = suspect(query.name as string, '');
+    let name = suspect(query.name as string, '');
 
-    if (name === '') return ng();
+    if (name === '') return ng('invalid parameter');
+
+    name = atob(name);
 
     const res = exifs.filter(x => x.name === name);
-    if (res.length === 0) return ng();
+    if (res.length === 0) return ng('nothing');
 
     return res[0].exif;
 })
