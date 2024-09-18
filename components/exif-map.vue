36d9919250cc24228895ad2b98e43473dd9557b5
Subilan <suyuanliang1995@gmail.com>
2024-09-18 22:43:35 +0800

    feat: openlayers map component

---
+++ components/exif-map.vue
@@ -0,0 +1,30 @@
+<template>
+  <client-only>
+    <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:400px">
+      <ol-view :center="center" :rotation="rotation" :zoom="zoom"
+               :projection="projection" />
+      <ol-tile-layer>
+        <ol-source-osm />
+      </ol-tile-layer>
+    </ol-map>
+  </client-only>
+</template>
+
+<script setup>
+const center = defineModel();
+const projection = ref('EPSG:4326')
+const zoom = ref(8)
+const rotation = ref(0)
+</script>
+
+<style lang="scss">
+.ol-attribution li {
+  display: flex;
+  align-items: baseline;
+  gap: 4px;
+
+  a::after {
+    content: none;
+  }
+}
+</style>
