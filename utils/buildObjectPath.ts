d1893cccd9be331962953b16bd52d8477cc84c94
Subilan <suyuanliang1995@gmail.com>
2025-02-08 14:41:03 +0800

    chore: name

--- utils/buildObjectPath.ts
+++ utils/buildObjectPath.ts
@@ -1,5 +1,5 @@
 import {OSSEndpoint} from "~/consts";
 
-export default function (prefix: string, objectName: string, quality: string = '', endpoint: string = OSSEndpoint) {
-    return `${endpoint}/public/frame/${prefix}/${objectName}${quality.length > 0 ? '?x-oss-process=image/resize,h_' + quality : ''}`;
+export default function (collection: string, objectName: string, quality: string = '', endpoint: string = OSSEndpoint) {
+    return `${endpoint}/public/frame/${collection}/${objectName}${quality.length > 0 ? '?x-oss-process=image/resize,h_' + quality : ''}`;
 }
