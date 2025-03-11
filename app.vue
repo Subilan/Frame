9b334ff8985fe0a8157de2915330d1b1a6524b72
Subilan <suyuanliang1995@gmail.com>
2025-03-11 21:30:03 +0800

    seo: lang

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
-  <main :lang="lang">
+  <Html :lang="lang">
+    <main :lang="lang">
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
   </main>
+  </Html>
 </template>
 
 <script setup>
@@ -12,4 +14,8 @@ import '@/assets/fonts/fira-sans/font.css';
 import '@/assets/fonts/fira-code/font.css';
 
 const lang = useLanguage();
+
+useHead({
+  titleTemplate: chunk => chunk ? `${chunk} | the frame` : 'the frame'
+})
 </script>
