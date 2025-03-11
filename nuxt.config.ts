3e2baa167a50f9e56964c1052c56d981d4f8147c
Subilan <suyuanliang1995@gmail.com>
2025-03-11 21:29:55 +0800

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,28 +2,32 @@ import svgLoader from 'vite-svg-loader';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-04-03',
+	compatibilityDate: '2024-04-03',
 
-    devtools: {
-        enabled: false
-    },
+	devtools: {
+		enabled: false
+	},
 
-    devServer: {
-        port: 7070
-    },
+	devServer: {
+		port: 7070
+	},
 
-    modules: ['@nuxt/image', 'nuxt-route-meta'],
+	modules: ['@nuxt/image', 'nuxt-route-meta'],
 
-    vite: {
-        plugins: [
-            svgLoader()
-        ]
-    },
+	vite: {
+		plugins: [svgLoader()]
+	},
 
-    plugins: [
-        {
-            src: '@/plugins/openlayer',
-            mode: 'client'
-        }
-    ]
-})
+	plugins: [
+		{
+			src: '@/plugins/openlayer',
+			mode: 'client'
+		}
+	],
+
+	app: {
+		head: {
+			link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+		},
+	}
+});
