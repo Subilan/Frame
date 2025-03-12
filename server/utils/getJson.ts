1c3d3dbb52ad594a59ff464d70c34bd1cd53294e
Subilan <suyuanliang1995@gmail.com>
2025-03-12 22:40:44 +0800

    fix path

--- server/utils/getJson.ts
+++ server/utils/getJson.ts
@@ -2,6 +2,6 @@ import * as fs from "fs/promises";
 import path from "node:path";
 
 export default async function (name: string) {
-    const content = await fs.readFile(process.cwd() + '/public/data/' + name);
+    const content = await fs.readFile(process.cwd() + '/data/' + name);
     return JSON.parse(content.toString());
 }
