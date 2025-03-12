98d8ce6d9c95a2c6a9f7e2b94f90279b0de98265
Subilan <suyuanliang1995@gmail.com>
2025-03-12 21:38:40 +0800

    fix: dayjs not supporting omission...

--- utils/getDayjs.ts
+++ utils/getDayjs.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone";
-import IsBetween from "dayjs/plugin/isBetween"
-import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone.js";
+import IsBetween from "dayjs/plugin/isBetween.js"
+import utc from "dayjs/plugin/utc.js";
 
 export default function () {
     dayjs.extend(utc);
