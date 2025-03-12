ae352bda4075a9084262690eead71f63f0ac84b2
Subilan <suyuanliang1995@gmail.com>
2025-03-12 21:31:21 +0800

    fix: dev url

--- utils/req.ts
+++ utils/req.ts
@@ -1,5 +1,5 @@
 import type { FrameResp } from "~/types";
 
 export default async function <T>(relativePath: string): Promise<FrameResp<T>> {
-    return await $fetch<FrameResp<T>>(import.meta.dev ? '' : 'https://photos.subilan.win' + relativePath);
+    return await $fetch<FrameResp<T>>((import.meta.dev ? '' : 'https://photos.subilan.win') + relativePath);
 }
