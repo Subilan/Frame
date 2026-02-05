acf0c2a2641917b99b617ff54aa1fe00b2fce7c0
Subilan <1843917482@qq.com>
2026-02-05 17:39:12 +0800

    update data

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [layout('./layouts/NavLayout.tsx', [
-    index('./routes/home.tsx'),
+    index('./routes/index.tsx'),
     route('/collection/*', './routes/collection.tsx')
 ])] satisfies RouteConfig;
