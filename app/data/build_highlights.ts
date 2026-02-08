8bf934fbe1e875b01bc2544c80554168a4f7cee7
Subilan <suyuanliang1995@gmail.com>
2026-02-08 18:44:21 +0800

    update

--- app/data/build_highlights.ts
+++ app/data/build_highlights.ts
@@ -11,6 +11,8 @@ import remarkExtractFrontmatter from 'remark-extract-frontmatter';
 import remarkFrontmatter from 'remark-frontmatter';
 import * as yaml from 'yaml';
 import path from 'path';
+import type { CollectionItem } from '~/data/types';
+import parseExifTime from '~/data/utils/parseExifTime';
 const SCRIPT_PATH = import.meta.dirname;
 
 const markdownFiles = await fs.readdir(SCRIPT_PATH + '/highlights');
@@ -44,6 +46,9 @@ export type HighlightItem = {
 	content: string;
 	imagePath?: string;
 	excerpt?: string;
+	imageInfo?: {
+		date?: Date;
+	};
 };
 
 type Frontmatter = {
@@ -52,18 +57,39 @@ type Frontmatter = {
 
 const parsed: HighlightItem[] = [];
 
+async function getImageInfo(simpleRepr?: string) {
+	if (!simpleRepr) return undefined;
+
+	const result = /^([A-Za-z0-9\-\/]+)\/(.*)$/.exec(simpleRepr);
+
+	if (!result) return undefined;
+
+	const collectionName = result[1].replaceAll('/', '~');
+	const collectionItems = (
+		await import(SCRIPT_PATH + '/dist/filetrees/' + collectionName + '.json')
+	).default as CollectionItem[];
+	const exif = collectionItems.find(item => item.name.endsWith(result[2]))?.exif;
+	if (!exif) return undefined;
+
+	return {
+		date: parseExifTime(exif.DateTime.value)
+	};
+}
+
 const parseTasks = markdownFiles.map(async filename => {
 	const document = (await fs.readFile(SCRIPT_PATH + '/highlights/' + filename)).toString();
 	const result = await applyPipeline(document);
 	const content = result.toString();
 	const excerpt = await getExcerpt(document);
 	const fm = result.data.fm as Partial<Frontmatter>;
+	const imageInfo = await getImageInfo(fm.path);
 	const data: HighlightItem = {
 		filename,
 		title: result.data.meta?.title || '',
 		content,
 		imagePath: fm.path,
-		excerpt: excerpt.length === content.length ? undefined : excerpt
+		excerpt: excerpt.length === content.length ? undefined : excerpt,
+		imageInfo
 	};
 	parsed.push(data);
 });
--- app/routes/highlights.tsx
+++ app/routes/highlights.tsx
@@ -30,6 +30,7 @@ export default function Highlights({ loaderData }: Route.ComponentProps) {
 	const { highlights } = loaderData;
 
 	const [currentHl, setCurrentHl] = useState<HighlightItemWithImagePath>(highlights[0]);
+	const [currentHlIndex, setCurrentHlIndex] = useState(0);
 	const [collapsed, setCollapsed] = useState<Record<string, boolean>>({});
 	const [swiper, setSwiper] = useState<SwiperClass>();
 
@@ -44,26 +45,13 @@ export default function Highlights({ loaderData }: Route.ComponentProps) {
 		[swiper]
 	);
 
-	const wheelListener = useCallback(
-		(e: WheelEvent) => {
-			if (e.deltaY < 0) {
-				swiper?.slidePrev();
-			} else {
-				swiper?.slideNext();
-			}
-		},
-		[swiper]
-	);
-
 	useEffect(() => {
 		window.addEventListener('keydown', keyboardListener);
-		window.addEventListener('wheel', wheelListener);
 
 		return () => {
 			window.removeEventListener('keydown', keyboardListener);
-			window.removeEventListener('wheel', wheelListener);
 		};
-	}, [keyboardListener, wheelListener]);
+	}, [keyboardListener]);
 
 	useEffect(() => {
 		highlights.map((x, i) => {
@@ -74,67 +62,79 @@ export default function Highlights({ loaderData }: Route.ComponentProps) {
 	}, [highlights]);
 
 	return (
-		<div className="w-full">
-			<Swiper
-				className="h-full bg-linear-0 from-0% from-black/50 to-transparent to-30%"
-				modules={[EffectFade]}
-				slidesPerView={1}
-				effect="fade"
-				loop
-				onSlideChange={swiper => setCurrentHl(highlights[swiper.activeIndex])}
-				onSwiper={swiper => setSwiper(swiper)}
-			>
-				{highlights.map(
-					(hl, i) =>
-						hl.imagePath && (
-							<SwiperSlide
-								key={hl.filename}
-								className="bg-center bg-cover bg-no-repeat bg-neutral-800"
-								style={{
-									height: 'calc(100dvh - 68px)',
-									backgroundImage: `url(${getOssUrlFromSimpleRepr(hl.imagePath + '?x-oss-process=image/resize,h_1080')})`
-								}}
-							>
-								<div className="flex items-end h-full p-10">
-									<div className="flex text-shadow-lg flex-col gap-3">
-										<h2 className="text-4xl">{hl.title}</h2>
-										<div
-											className="[&_h1]:hidden [&_p]:my-2 max-w-[700px] text-lg"
-											dangerouslySetInnerHTML={{
-												__html:
-													collapsed[i] && hl.excerpt
-														? hl.excerpt
-														: hl.content
-											}}
-										></div>
-										{hl.excerpt && (
-											<a
-												onClick={() =>
-													setCollapsed(clp => ({ ...clp, [i]: !clp[i] }))
-												}
-												className="inpage-link"
-											>
-												{collapsed[i] ? '展开' : '收起'}
-											</a>
+		<div className="max-w-[1200px] mx-auto">
+			<div className="h-[70dvh] relative">
+				<Swiper
+					className="h-full"
+					modules={[EffectFade]}
+					slidesPerView={1}
+					effect="fade"
+					loop
+					onSlideChange={swiper => {
+						setCurrentHlIndex(swiper.realIndex);
+						setCollapsed(clp => ({ ...clp, [swiper.realIndex]: true }));
+						setCurrentHl(highlights[swiper.realIndex]);
+					}}
+					onSwiper={swiper => setSwiper(swiper)}
+				>
+					{highlights.map(
+						(hl, i) =>
+							hl.imagePath && (
+								<SwiperSlide key={hl.filename} className="bg-neutral-900">
+									<img
+										className="h-full block mx-auto object-contain shadow-2xl"
+										src={getOssUrlFromSimpleRepr(
+											hl.imagePath + '?x-oss-process=image/resize,h_1080'
 										)}
-									</div>
-								</div>
-							</SwiperSlide>
-						)
+									/>
+								</SwiperSlide>
+							)
+					)}
+				</Swiper>
+				<button onClick={() => swiper?.slidePrev()} className="swiper-nav-button -left-20">
+					<ArrowLeftIcon />
+				</button>
+				<button onClick={() => swiper?.slideNext()} className="swiper-nav-button -right-20">
+					<ArrowRightIcon />
+				</button>
+			</div>
+
+			<div className="flex flex-col items-center gap-3 my-5">
+				<h2 className="text-2xl">{currentHl.title}</h2>
+				{currentHl.imageInfo && (
+					<h3 className="text-xl text-neutral-500">{new Date(currentHl.imageInfo.date ?? 0).toLocaleString()}</h3>
 				)}
-			</Swiper>
-			<NavLink
-				to={getFrameUrlFromSimpleRepr(currentHl.imagePath)}
-				className="absolute shadow-lg right-10 bottom-10 z-10 text-xl rounded-full bg-sky-200 hover:scale-105 active:scale-95 transition-all text-sky-900 py-3 px-5 "
-			>
-				在相册中查看 <ArrowUpRightIcon className="inline-block" />
-			</NavLink>
-			<button onClick={() => swiper?.slidePrev()} className="swiper-nav-button left-10">
-				<ArrowLeftIcon />
-			</button>
-			<button onClick={() => swiper?.slideNext()} className="swiper-nav-button right-10">
-				<ArrowRightIcon />
-			</button>
+				<div
+					className="[&_h1]:hidden [&_p]:my-2 text-center max-w-[700px] text-lg"
+					dangerouslySetInnerHTML={{
+						__html:
+							collapsed[currentHlIndex] && currentHl.excerpt
+								? currentHl.excerpt
+								: currentHl.content
+					}}
+				></div>
+				<div className="flex items-center gap-3">
+					{currentHl.excerpt && (
+						<a
+							onClick={() =>
+								setCollapsed(clp => ({
+									...clp,
+									[currentHlIndex]: !clp[currentHlIndex]
+								}))
+							}
+							className="inpage-link"
+						>
+							{collapsed[currentHlIndex] ? '展开' : '收起'}描述
+						</a>
+					)}
+					<NavLink
+						to={getFrameUrlFromSimpleRepr(currentHl.imagePath)}
+						className="inpage-link"
+					>
+						在相册中查看
+					</NavLink>
+				</div>
+			</div>
 		</div>
 	);
 }
